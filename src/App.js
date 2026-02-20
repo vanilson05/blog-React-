66e1f795808e6300c6490b4542e8d9bb76258125
vanilson05 <souzavanilson496@gmail.com>
2026-02-20 05:43:54 -0300

    stile new end posts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import Post from './components/Posts';
 
 function App() {
   return (
     <>
       <Header />
       <h1>meu blog</h1>
+      <Post />
     </>
   );
 }
---
+++ src/components/Article/index.js
@@ -0,0 +1,16 @@
+import post from "../Post";
+
+const Article = () => {
+    return (
+        <section className="container">
+            <h3>Articles</h3>
+            <div className="container-posts">
+
+            </div>
+
+            <post />
+        </section>
+    )
+}
+
+export default Article;
